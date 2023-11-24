6dc4271c9f503f4e50ebd4fe5d0991d60370ddfb
KateM_zz <zupperu@gmail.com>
2023-11-24 03:29:27 +0300

    check

---
+++ src/ch.py
@@ -0,0 +1,1 @@
+h = "Normally you always want the pre-commit hooks to run when committing, but in some circumstances you might want to do one without the other. These two scenarios are covered below:"
--- src/check.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI
-
-app = FastAPI()
-
-
-h = "I wuld have expected the install command to create an isolated environment, install the build requirements and perform the build in that isolated environment, and then install the produced artifact in my current environment."
-
-
-print(h)
