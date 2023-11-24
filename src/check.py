f0730dffa6f53680ddfeef662dcdb0bffcab0e05
KateM_zz <zupperu@gmail.com>
2023-11-24 03:06:38 +0300

    new version

--- src/check.py
+++ src/check.py
@@ -3,7 +3,7 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
-h = "I would have expected the install command to create an isolated environment, install the build requirements and perform the build in that isolated environment, and then install the produced artifact in my current environment."
+h = "I wuld have expected the install command to create an isolated environment, install the build requirements and perform the build in that isolated environment, and then install the produced artifact in my current environment."
 
 
 print(h)
