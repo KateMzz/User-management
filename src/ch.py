fbc7e766bec5d31030a1713b1a38103361e1d149
KateM_zz <zupperu@gmail.com>
2023-11-24 03:34:46 +0300

    check

--- src/ch.py
+++ src/ch.py
@@ -1,1 +1,5 @@
-h = "Normally you always want the pre-commit hooks to run when committing, but in some circumstances you might want to do one without the other. These two scenarios are covered below:"
+def foo():
+    pass
+
+
+print("hey")
