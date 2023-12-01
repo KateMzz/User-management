83be098dbb9ae180188ec31369bb067d61b6a945
KateM_zz <zupperu@gmail.com>
2023-12-01 21:52:56 +0300

    pulled updates from master

---
+++ src/controllers/api_v1/user/ctrl_patch_user_me.py
@@ -0,0 +1,29 @@
+from fastapi import APIRouter, Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+from starlette import status
+from starlette.responses import JSONResponse
+
+from src.repositories.repo_user import UserRepository
+from src.schemas.response import IResponse
+from src.schemas.sch_user import AccessToken, UserDetail
+from src.services.auth.auth_service import AuthService
+from utils.db_connection import get_async_session
+
+router = APIRouter()
+
+
+@router.patch(
+    path="/user/me",
+    status_code=status.HTTP_200_OK,
+    response_model=IResponse[UserDetail],
+    responses={200: {"description": "Updated"}, 400: {"description": "Bad request"}},
+)
+async def create_new_user(
+    session: AsyncSession = Depends(get_async_session), token=Depends(AccessToken)
+) -> JSONResponse:
+    user_id = await AuthService(session).get_user_id_from_token(token=token.token)
+    user = await UserRepository(session).get_user_info(user_id)
+    result = await UserRepository(session).row_to_dict(row=user)
+
+    # return IResponse(payload=UserDetail(**result), status_code=200, message="Success")
+    return JSONResponse(content=result, status_code=200)
